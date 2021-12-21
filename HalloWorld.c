a73801b860f7ea4056c47e731279eaeaca88a87a
hheiko <1116186+hheiko@users.noreply.github.com>
2021-12-21 15:20:58 +0100

    first commit

---
+++ HalloWorld.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : HalloWorld.c
+ Author      : heiko
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	return EXIT_SUCCESS;
+}
